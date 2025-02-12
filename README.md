e4bfd512713ecdcffbd8379d45b87f864e5d4db9
geraldo ferreira <geraldosousadev@gmail.com>
2025-02-12 09:29:13 -0300

    config session instance database

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # donation-api-with-fastapi
 donation api so that anyone can donate and register who did
+
+## Commands to run in development
+```
+    uvicorn app.main:app --reload
+```
--- app/database.py
+++ app/database.py
@@ -1,9 +1,21 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///./db.db', echo=True)
-
+SessionLocal: sessionmaker[Session] = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine
+)
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 def init_db():
