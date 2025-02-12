da6cae2694f7c7c7129aa55a93a4937aaf2223e3
geraldo ferreira <geraldosousadev@gmail.com>
2025-02-11 21:00:23 -0300

    created table user

--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
-from sqlalchemy import create_engine, engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = 'sqlite:///./db.db'
+engine = create_engine('sqlite:///./db.db', echo=True)
 
-engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 Base = declarative_base()
+
+Base.metadata.create_all(bind=engine)
