1b1eaacaad3aa7c6599abf4ee6aa6980e11a0362
geraldo ferreira <geraldosousadev@gmail.com>
2025-02-12 09:01:37 -0300

    add field in table user active and password

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import DateTime, String, Integer, func
+from sqlalchemy import Boolean, DateTime, String, Integer, func
 from sqlalchemy.orm import Mapped, mapped_column
 from app.database import Base
 
@@ -11,6 +11,8 @@ class User(Base):
         Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(30))
     age: Mapped[int] = mapped_column(Integer, nullable=False)
+    active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
     email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    passworrd: Mapped[str] = mapped_column(String, )
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), nullable=False)
